d644b3b3e25a2cfa507ca2e2c394b62cc4cd4bb8
tahyeokkim89 <wskth0930@gmail.com>
2024-12-26 08:48:36 +0900

    firebase Auth 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import Router from './shared/Router'
+import Router from './shared/Router';
+import { app } from "firebaseApp";
+import { getAuth } from "firebase/auth"
+import { useState } from 'react';
 
 function App() {
-  return <Router/>
+  const auth = getAuth(app)
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  )
+  return <Router isAuthenticated={isAuthenticated}/>
 }
 
 export default App;
