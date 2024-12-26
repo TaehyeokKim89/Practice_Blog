7bfeae3302c3cb37690ce762d845876f2f654faa
tahyeokkim89 <wskth0930@gmail.com>
2024-12-26 11:26:06 +0900

    Auth 컨텍스트 생성 프로필에 적용

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,28 +1,29 @@
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
-import { Link } from "react-router-dom";
+import { useContext } from "react";
 import { toast } from "react-toastify";
 
-const Profile = () => {
-  const auth = getAuth(app)
-
-  const onSignOut =  async () => {
-    try {
-      const auth = getAuth(app)
-    await signOut(auth)
-    toast.success("로그아웃 되었습니다.")
-    } catch (e: any) {
-      console.log(e)
-      toast.error(e)
-    }
+const onSignOut =  async () => {
+  try {
+    const auth = getAuth(app)
+  await signOut(auth)
+  toast.success("로그아웃 되었습니다.")
+  } catch (e: any) {
+    console.log(e)
+    toast.error(e)
   }
+}
+
+const Profile = () => {
+  const { user } = useContext(AuthContext)
 
   return <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image"/>
         <div>
-          <div className="profile__email">{auth?.currentUser?.email}</div>
-          <div className="profile__name">{auth?.currentUser?.displayName || '사용자'}</div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">{user?.displayName || '사용자'}</div>
         </div>
       </div>
       <div role='presentation' className="profile__logout" onClick={onSignOut}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import './index.css';
-import App from './App';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import "./index.css";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthContextProvider } from "context/AuthContext";
 
-const rootElement = document.getElementById('root'); // 전용 컨테이너 요소를 선택
+const rootElement = document.getElementById("root"); // 전용 컨테이너 요소를 선택
 if (rootElement) {
   const root = createRoot(rootElement); // 전용 컨테이너를 사용하여 React 앱을 초기화
   root.render(
-    <Router>
-      <App />
-    </Router>
-);
+    <AuthContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthContextProvider>
+  );
 }
