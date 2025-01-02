19d72ce3f8d5e76da682a99c62c8a49851bce5d2
tahyeokkim89 <wskth0930@gmail.com>
2025-01-03 08:12:08 +0900

    PostDetail 데이터 불러오기 완료

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,58 +1,57 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { PostProps } from "./PostList";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import Loader from "./Loader";
 
 const PostDetail = () => {
+  const[post, setPost] = useState<PostProps | null>(null)
+  const params = useParams()
+
+  const getPost = async (id:string) => {
+    if(id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+      setPost({id: docSnap.id, ...(docSnap.data() as PostProps)})
+    }
+  }
+
+  const handleDelete = () => {
+    console.log('delete!')
+  }
+
+  useEffect(()=>{
+    if(params?.id) getPost(params?.id);
+  },[params?.id])
+
   return (
     <div className="post__detail">
+      {post? (
       <div className="post__box">
-        <div className="post__title">Lorem ipsum dolor sit amet</div>
+        <div className="post__title">{post?.title}</div>
         <div className="post__profile-box">
           <div className="post__profile" />
-          <div className="post__author-name">블로그 제목</div>
-          <div className="post__date">2023.07.08 토요일</div>
+          <div className="post__author-name">{post?.email}</div>
+          <div className="post__date">{post?.createdAt}</div>
         </div>
 
         <div className="post__utils-box">
-          <div className="post__delete">삭제</div>
+          <div 
+            className="post__delete" 
+            role="presentation"
+            onClick={handleDelete}>삭제</div>
           <div className="post__edit">
             <Link to={`/posts/edit/1`} >수정</Link>
           </div>
         </div>
-        <div className="post__text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          eleifend elementum est et ultricies. Nam vel urna pretium, bibendum
-          est a, convallis tellus. Etiam nec magna congue, egestas lacus et,
-          porttitor sapien. Nullam volutpat tortor a risus ultricies bibendum.
-          Mauris neque massa, faucibus vel consectetur sit amet, malesuada in
-          felis. In vitae sodales lectus. Curabitur iaculis porttitor dui, vitae
-          eleifend lacus tincidunt in. Sed ut nisl nunc. Mauris tincidunt quis
-          dolor nec facilisis. Duis accumsan urna eu orci hendrerit, et pharetra
-          orci cursus. Integer ac velit vestibulum, aliquet risus eu,
-          scelerisque neque. Duis a leo nibh. Aenean blandit sem in nulla
-          finibus, at posuere dui porttitor. Nulla et arcu vitae lacus varius
-          aliquam. Maecenas vitae felis lacus. Sed iaculis tortor lacus, sed
-          porttitor velit cursus at. Suspendisse dapibus elementum orci, vitae
-          sagittis tellus laoreet nec. Duis tempus massa a dolor dignissim, non
-          molestie dolor porta. Vestibulum suscipit varius dolor in blandit.
-          Morbi eu arcu id est viverra tincidunt. Nulla facilisi. Donec
-          elementum purus at tortor aliquam mattis. Etiam sed velit vel elit
-          auctor convallis ut eget risus. Phasellus et ex mauris. Nullam eu
-          lorem mattis, accumsan arcu vitae, egestas leo. Pellentesque
-          condimentum ex leo, et consequat diam imperdiet non. Cras quis tellus
-          a sapien mattis lobortis vel ac ante. Donec venenatis at orci quis
-          imperdiet. Cras blandit, dolor quis scelerisque rhoncus, orci dolor
-          vestibulum enim, egestas eleifend nibh magna eu metus. Mauris maximus
-          bibendum nulla, in placerat urna porta eleifend. Nunc congue a mauris
-          in vestibulum. Nullam luctus malesuada turpis vel lobortis. Fusce
-          congue ante vitae metus iaculis, id lobortis enim ornare. Fusce
-          blandit tincidunt urna. Maecenas elementum gravida felis quis dapibus.
-          Curabitur in eros in tortor fermentum mollis vel id mauris. Proin
-          tincidunt blandit ex eget tempus. Pellentesque laoreet eget magna in
-          luctus. Vivamus tincidunt vel ipsum in finibus. Maecenas convallis
-          nisl lorem, eget efficitur justo luctus vitae. Pellentesque dapibus
-          accumsan diam, sed elementum erat eleifend ut. Donec interdum non ex
-          at suscipit.
+        <div className="post__text post__text--pre-wrap">
+          {post?.content}
         </div>
       </div>
+      ) : (
+        <Loader/>
+      )}
     </div>
   );
 };
